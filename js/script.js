4b21eee6cec2e6bcb30ed9b7fe9ac18278e7186f
Kristina-code <55065132+Kristina-code@users.noreply.github.com>
2019-11-21 19:08:15 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,17 +3,7 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
-
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
 
 var quotes = [
   {
@@ -60,24 +50,21 @@ var quotes = [
 
     quote: "I failed my way to success.",
 
-    source: "Thomas Edison",
+    source:"Thomas Edison",
 
-    citation: " Unknown "
+    citation: "Unknown"
 
 
-  }
+  },
 
 ];
 
 
 
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Use the random number to `return` a random quote object from the `quotes` array. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
+/*** 
+   - I used this website as guideline to create the getRandomQuote function  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
 ***/
-
 
 
 function getRandomQuote() 
@@ -91,7 +78,7 @@ function getRandomQuote()
 };
 
 
-//Create the `printQuote` function to: Call the `getRandomQuote` function and assign it to a variable.
+//printQuote function
 
 
 function printQuote() {
@@ -99,14 +86,7 @@ function printQuote() {
   phrases = getRandomQuote();
 
 
-  // Create a variable for the html string and set it equal to an empty string.
-
   randomGenerator = "";
-
-  // Use the html template in the instructions or the markup in the index.html file, AND the random quote vairable to build your html string.
-  //teamtreehouse.com/projects/a-random-quote-generator
-  //  Add the quote and source section to the html string.
-
 
   randomGenerator += `<p class="quote"> ` + phrases.quote + '</p>';
 
@@ -114,37 +94,27 @@ function printQuote() {
 
 
 
-
-  //   Use an if statement to check for the citation property before adding it to the html string.
-  //   https://www.w3schools.com/java/java_conditions.asp
-
+  // Referenced:  https://www.w3schools.com/java/java_conditions.asp to learn how assit with if statements
 
   if (phrases.citation) {
 
     randomGenerator += `<p class="citation">`  + phrases.citation + '</p>';
+
+    
   }; 
 
+   if (phrases.year) {
+
+    randomGenerator += `<p class="year"` + phrases.year + '</p>'; 
   
-  //  Use an if statement to check for the year property before adding it to the html string.
-
-
-  if (phrases.year) {
-
-    randomGenerator += `<p class="year"` + phrases.year + '</p>';
-
-
   }
 
   randomGenerator += '</p';
 
   document.getElementById('quote-box').innerHTML = randomGenerator;
 
-  
   var randomNumber = Math.floor(Math.random() * quotes.length);
-  
-
- // Target the div with the id of quote-box, and set its innerHTML equal to the HTML string that’s been assembled. 
-    // Set the `innerhtml` of the `quote-box` div to the html string.      
+        
 
     var printDiv = document.getElementById('quote-box');
 
@@ -152,7 +122,7 @@ function printQuote() {
 
 }
 
- //  When the "Show another quote" button is clicked, the event listener below will be triggered, and it will call, or "invoke", the `printQuote` function.
+ // " When the "Show another quote" button is clicked, the event listener below will be triggered, and it will call, or "invoke", the `printQuote` function.""
   
 
     document.getElementById('loadQuote').addEventListener("click", printQuote, false);
